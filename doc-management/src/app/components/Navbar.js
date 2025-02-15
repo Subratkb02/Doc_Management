9e213ee97bcb09aee4c33357a909d8d02ba33665
Subratkb02 <worksubrat2002@gmail.com>
2025-02-15 17:35:38 +0530

    Updated files

---
+++ doc-management/src/app/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Link from 'next/link';
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light shadow top-0">
+            <div className="container">
+                <Link href="/">
+                    <a className="navbar-brand">Doc Management System</a>
+                </Link>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link href="/documents">
+                                <a className="nav-link">Documents</a>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="/upload">
+                                <a className="nav-link">Upload</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+    )
+};
